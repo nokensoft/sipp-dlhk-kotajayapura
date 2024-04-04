0e47d3329aa1e5c22900759f9d59347bb2cc9e48
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-04-05 03:19:37 +0900

    Grafik Berdasarkan Pangkat/Golongan

--- resources/views/AdminMaster/dasbor.blade.php
+++ resources/views/AdminMaster/dasbor.blade.php
@@ -5,8 +5,88 @@
     <h4 class="mb-1">Hello, Admin Master!</h4>
     <p>Selamat datang di halaman utama.</p>
 </div>
+
 <div class="flex flex-col xl:flex-row gap-4">
     <div class="flex flex-col gap-4 flex-auto">
+
+        <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4">
+            <div class="card card-layout-frame">
+                <div class="card-body">
+                    <div class="flex items-center gap-4">
+                        <span class="avatar avatar-rounded bg-indigo-100 text-indigo-600 dark:bg-indigo-500/20 dark:text-indigo-100 avatar-lg text-3xl" data-avatar-size="55">
+                            <span class="avatar-icon">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
+                                </svg>
+                            </span>
+                        </span>
+                        <div>
+                            <div class="flex gap-1.5 items-end mb-2">
+                                <h3 class="font-bold leading-none">63</h3>
+                                <p class="font-semibold">ASN</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card card-layout-frame">
+                <div class="card-body">
+                    <div class="flex items-center gap-4">
+                        <span class="avatar avatar-rounded bg-cyan-100 text-cyan-600 dark:bg-cyan-500/20 dark:text-cyan-100 avatar-lg text-3xl" data-avatar-size="55">
+                            <span class="avatar-icon">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
+                                </svg>
+                            </span>
+                        </span>
+                        <div>
+                            <div class="flex gap-1.5 items-end mb-2">
+                                <h3 class="font-bold leading-none">25</h3>
+                                <p class="font-semibold">Non ASN</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card card-layout-frame">
+                <div class="card-body">
+                    <div class="flex items-center gap-4">
+                        <span class="avatar avatar-rounded bg-emerald-100 text-emerald-600 dark:bg-emerald-500/20 dark:text-emerald-100 avatar-lg text-3xl" data-avatar-size="55">
+                            <span class="avatar-icon">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
+                                </svg>
+                            </span>
+                        </span>
+                        <div>
+                            <div class="flex gap-1.5 items-end mb-2">
+                                <h3 class="font-bold leading-none">49</h3>
+                                <p class="font-semibold">Laki-Laki</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card card-layout-frame">
+                <div class="card-body">
+                    <div class="flex items-center gap-4">
+                        <span class="avatar avatar-rounded bg-amber-100 text-amber-600 dark:bg-amber-500/20 dark:text-amber-100 avatar-lg text-3xl" data-avatar-size="55">
+                            <span class="avatar-icon">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
+                                </svg>
+                            </span>
+                        </span>
+                        <div>
+                            <div class="flex gap-1.5 items-end mb-2">
+                                <h3 class="font-bold leading-none">12</h3>
+                                <p class="font-semibold">Perempuan</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <div class="card card-layout-frame">
             <div class="card-body">
@@ -64,6 +144,21 @@
                 </div>
             </div>
 
+        </div>
+    </div>
+</div>
+
+<div class="flex flex-col xl:flex-row gap-4">
+    <div class="flex flex-col gap-4 flex-auto">
+        <div class="card card-layout-frame">
+            <div class="card-body">
+                <div class="flex sm:flex-row flex-col md:items-center justify-center mb-6 gap-4">
+                    <h4>Grafik Berdasarkan Pangkat/Golongan</h4>
+                </div>
+                <div>
+                    <div id="task-overview-chart"></div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/AdminMaster/layouts/includes/sidebar.blade.php
+++ resources/views/AdminMaster/layouts/includes/sidebar.blade.php
@@ -9,7 +9,7 @@
 
             <div class="menu-group">
                 <div class="menu-title menu-title-transparent">
-                    Menu
+                    Menu Utama
                 </div>
                 <ul>
                     <li data-menu-item="menu-item-icon" class="menu-item menu-item-single mb-2">
@@ -20,7 +20,34 @@
                             <span class="menu-item-text">Dasbor</span>
                         </a>
                     </li>
-
+                    <li data-menu-item="menu-item-icon" class="menu-item menu-item-single mb-2">
+                        <a class="menu-item-link" href="{{route('adminmaster.dasbor')}}">
+                            <svg class="menu-item-icon" stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                            </svg>
+                            <span class="menu-item-text">Laporan Harian</span>
+                        </a>
+                    </li>
+                    <div class="menu-title menu-title-transparent">
+                        Manajemen Pegawai
+                    </div>
+                    <ul>
+                        <li data-menu-item="menu-item-icon" class="menu-item menu-item-single mb-2">
+                            <a class="menu-item-link" href="{{route('adminmaster.dasbor')}}">
+                                <svg class="menu-item-icon" stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                                </svg>
+                                <span class="menu-item-text">ASN</span>
+                            </a>
+                        </li>
+                        <li data-menu-item="menu-item-icon" class="menu-item menu-item-single mb-2">
+                            <a class="menu-item-link" href="{{route('adminmaster.dasbor')}}">
+                                <svg class="menu-item-icon" stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                                </svg>
+                                <span class="menu-item-text">Non ASN</span>
+                            </a>
+                        </li>
                 </ul>
             </div>
         </nav>
