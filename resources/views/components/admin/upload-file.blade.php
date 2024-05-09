0eb9b6418a4a5f526ba0bde4d8c234bab984cd3c
obipranata <obiimanuelpranata@gmal.com>
2024-05-09 14:44:53 +0700

    feat: add button delete file #10

--- resources/views/components/admin/upload-file.blade.php
+++ resources/views/components/admin/upload-file.blade.php
@@ -10,10 +10,11 @@
 @if($isDisabled)
     <div class="border-2 border-gray-400 rounded-xl h-[300px] cursor-default flex p-2 justify-center items-center">
         @if($isPdf)
-            <iframe id="iframe-{{$id}}" src="{{ asset('storage/files/ktp.pdf') }}" class="w-full h-full" type="text/pdf">Open the pdf!</iframe>
+            <iframe id="iframe-{{$id}}" src="{{ $img != '' ? asset('storage/'.$img) : asset('assets/img/others/upload.png')}}" class="w-full h-full" type="text/pdf">Open the pdf!</iframe>
         @else
             <div class="flex flex-col">
                 <img src="{{ $img != '' ? asset('storage/'.$img) : asset('assets/img/others/upload.png')}}" alt="" class="mx-auto max-h-52" id="img-{{$id}}">
+                <p class="mt-1 opacity-60">Support: jpeg, png, pdf</p>
                 @if($img != '')
                     <button class="bg-[#4F46E5] text-white py-2 px-4 rounded-xl mt-2" type="button" wire:click.prevent="$dispatch('download', { img: {{json_encode($img)}} })">
                         <i class="fa-solid fa-arrow-down"></i>
@@ -24,13 +25,13 @@
         @endif
     </div>
 @else
-    <div class="upload upload-draggable h-[267px] relative @if(!$isDisabled) hover:border-primary-600 @endif @error($name) border-red-500 hover:border-red-500 @enderror">
+    <div class="upload upload-draggable h-[290px] relative @if(!$isDisabled) hover:border-primary-600 @endif @error($name) border-red-500 hover:border-red-500 @enderror">
         <div
             wire:ignore
             x-data="{
                     id: @js($id),
                     isPdf: @js($isPdf),
-                    img: @js($img),
+                    file: @js($img),
                     handleImageChange() {
                         const inputElement = document.getElementById(this.id);
                         const file = inputElement.files[0]; // Ambil file dari input
@@ -47,25 +48,47 @@
                                     document.getElementById(`img-${this.id}`).src = e.target.result;
                                     this.isPdf = false;
                                 }
+                                this.file = e.target.result;
                             };
 
                             reader.readAsDataURL(file);
                         }
+                    },
+                    deleteFile(){
+                        const protocol = window.location.protocol+'//';
+                        const hostname= window.location.hostname;
+                        const port= window.location.port == '' ? '' : ':'+window.location.port;
+                        const url = protocol+hostname+port;
+                        console.log(url)
+                        const inputElement = document.getElementById(this.id);
+                        const extension = inputElement.value.split('.')[1];
+                        if (extension === 'pdf'){
+                           document.getElementById(`iframe-${this.id}`).src = url+'/assets/img/others/upload.png';
+                           this.isPdf = true;
+                        }else{
+                           document.getElementById(`img-${this.id}`).src = url+'/assets/img/others/upload.png';
+                           this.isPdf = false;
+                        }
+                        this.file = '';
                     }
                  }"
         >
             <label class="form-label"></label>
             <div>
-                <input class="upload-input draggable" type="file" id="{{$id}}" @change="handleImageChange" wire:model="{{$name}}">
-                <div class="my-16 text-center">
-                    <iframe x-show="isPdf" id="iframe-{{$id}}" src="{{ asset('storage/files/ktp.pdf') }}" class="w-full" type="text/pdf">Open the pdf!</iframe>
-                    <img x-show="!isPdf" src="{{ $img != '' ? asset('storage/'.$img) : asset('assets/img/others/upload.png')}}" alt="" class="mx-auto max-h-52" id="img-{{$id}}">
+                <input class="upload-input draggable z-20" type="file" id="{{$id}}" @change="handleImageChange" wire:model="{{$name}}">
+                <div class="my-16 text-center relative">
+                    <iframe x-show="isPdf" id="iframe-{{$id}}" src="{{ $img != '' ? asset('storage/'.$img) : asset('assets/img/others/upload.png')}}" class="w-full text-center" type="text/pdf">Open the pdf!</iframe>
+                    <img x-show="!isPdf" src="{{ $img != '' ? asset('storage/'.$img) : asset('assets/img/others/upload.png')}}" alt="" class="mx-auto max-h-36" id="img-{{$id}}">
                     @if(!$isDisabled)
                         <p class="font-semibold">
                             <span class="text-gray-800">drop file {{$label}} anda disini, atau</span>
                             <span class="text-blue-500">pilih</span>
                         </p>
                         <p class="mt-1 opacity-60">Support: jpeg, png, pdf</p>
+                        <button x-show="file != ''" class="bg-red-500 text-white py-2 px-4 rounded-xl left-0 right-0 absolute  z-50" type="button" @click="deleteFile()" wire:click.prevent="$dispatch('delete-file', { name: id })">
+                            <i class="fa-solid fa-trash"></i>
+                            Hapus
+                        </button>
                     @endif
                 </div>
             </div>
