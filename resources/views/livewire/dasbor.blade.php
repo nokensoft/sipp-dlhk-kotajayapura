3d4c0f7b83c2e61b0f502c764c3da8588a771bc4
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-08-03 14:37:16 +0900

    updated: Dasbor (tambah card-layout-frame pada box di halaman dasbor

--- resources/views/livewire/dasbor.blade.php
+++ resources/views/livewire/dasbor.blade.php
@@ -13,34 +13,39 @@
             <div class="flex flex-col xl:flex-row gap-4">
                 <div class="flex flex-col gap-4 flex-auto">
 
+                    <div>
+                        <x-chart class="w-full card card-layout-frame" />
+                    </div>
+
                     <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4">
-                        
+
                         <div class="card card-layout-frame">
-                            <div class="card-body">
-                                <div class="flex items-center gap-4">
-                                    <span
-                                        class="avatar avatar-rounded bg-indigo-100 text-indigo-600 avatar-lg text-3xl"
-                                        data-avatar-size="55">
-                                        <span class="avatar-icon">
-                                            <svg stroke="currentColor" fill="currentColor" stroke-width="0"
-                                                viewBox="0 0 20 20" aria-hidden="true" height="1em" width="1em"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd"
-                                                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                                                    clip-rule="evenodd"></path>
-                                            </svg>
+                                <div class="card-body">
+                                    <div class="flex items-center gap-4">
+                                        <span
+                                            class="avatar avatar-rounded bg-indigo-100 text-indigo-600 avatar-lg text-3xl"
+                                            data-avatar-size="55">
+                                            <span class="avatar-icon">
+                                                <svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                                                    viewBox="0 0 20 20" aria-hidden="true" height="1em" width="1em"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd"
+                                                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                                                        clip-rule="evenodd"></path>
+                                                </svg>
+                                            </span>
                                         </span>
-                                    </span>
-                                    <div>
-                                        <div class="flex gap-1.5 items-end mb-2">
-                                            <h3 class="font-bold leading-none">{{$totalASN}}</h3>
-                                            <p class="font-semibold">ASN</p>
+                                        <div>
+                                            <div class="flex gap-1.5 items-end mb-2">
+                                                <h3 class="font-bold leading-none">{{$totalASN}}</h3>
+                                                <p class="font-semibold">ASN</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <!-- end item -->
+                            <!-- end item -->
+
 
                         <div class="card card-layout-frame">
                             <div class="card-body">
@@ -342,15 +347,15 @@
                     </div>
 
                     <div>
-                        <x-chart class="w-full" title="Jumlah Petugas Berdasarkan Bidang Kerja" :data="$bidangs"/>
+                        <x-chart class="w-full card card-layout-frame" title="Jumlah Petugas Berdasarkan Bidang Kerja" :data="$bidangs"/>
                     </div>
                     <div>
-                        <x-chart class="w-full" title="Jumlah Petugas Berdasarkan Lokasi Kerja" :data="$lokasi"/>
+                        <x-chart class="w-full card card-layout-frame" title="Jumlah Petugas Berdasarkan Lokasi Kerja" :data="$lokasi"/>
                     </div>
                     <div class="flex gap-4">
-                        <x-chart class="w-1/3" title="Jumlah Petugas Berdasarkan Status Pegawai" :data="$statusPegawai" height="400" fontSize="12"/>
-                        <x-chart class="w-1/3" title="Jumlah Petugas Berdasarkan Suku" :data="$suku" height="400" fontSize="14"/>
-                        <x-chart class="w-1/3" title="Jumlah Petugas Berdasarkan Jenis Kelamin" :data="$jenisKelamins" height="400" fontSize="12"/>
+                        <x-chart class="w-1/3 card card-layout-frame" title="Jumlah Petugas Berdasarkan Status Pegawai" :data="$statusPegawai" height="400" fontSize="12"/>
+                        <x-chart class="w-1/3 card card-layout-frame" title="Jumlah Petugas Berdasarkan Suku" :data="$suku" height="400" fontSize="14"/>
+                        <x-chart class="w-1/3 card card-layout-frame" title="Jumlah Petugas Berdasarkan Jenis Kelamin" :data="$jenisKelamins" height="400" fontSize="12"/>
                     </div>
 
                 </div>
